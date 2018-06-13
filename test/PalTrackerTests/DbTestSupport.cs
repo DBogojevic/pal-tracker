6eabc2f18073c60fa1f6a365b07b4db539631d86
Mike Gehard <mgehard@pivotal.io>
2018-06-13 09:14:20 -0600

    Adding test support files

--- test/PalTrackerTests/DbTestSupport.cs
+++ test/PalTrackerTests/DbTestSupport.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using System.Collections.Generic;
 using System.Linq;
 using MySql.Data.MySqlClient;
@@ -43,4 +44,51 @@ namespace PalTrackerTests
             return result;
         }
     }
+=======
+﻿using System.Collections.Generic;
+using System.Linq;
+using MySql.Data.MySqlClient;
+
+namespace PalTrackerTests
+{
+    public static class DbTestSupport
+    {
+        public const string TestDbConnectionString = "server=127.0.0.1;uid=tracker_dotnet;pwd=password;database=tracker_dotnet_test";
+
+        public static IList<IDictionary<string, object>> ExecuteSql(string sql)
+        {
+            var result = new List<IDictionary<string, object>>();
+
+            using (var connection = new MySqlConnection(TestDbConnectionString))
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = sql;
+
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.HasRows)
+                        {
+                            while (reader.Read())
+                            {
+                                var rowData = Enumerable.Range(0, reader.FieldCount)
+                                    .ToDictionary(reader.GetName, reader.GetValue);
+
+                                result.Add(rowData);
+                            }
+
+                            reader.NextResult();
+                        }
+                    }
+                }
+
+                connection.Close();
+            }
+
+            return result;
+        }
+    }
+>>>>>>> efe61da... Add tests for Database lab
 }
