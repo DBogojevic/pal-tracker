b5c60417a425ce4c7725e47e3a9abc51a7977163
Dejan Bogojevic <dejan.bogojevic@dell.com>
2019-01-09 16:50:02 -0500

    health-monitoring assignment

--- src/PalTracker/Startup.cs
+++ src/PalTracker/Startup.cs
@@ -11,6 +11,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
+using Steeltoe.Management.CloudFoundry;
+using Steeltoe.Management.Endpoint.CloudFoundry;
+using Steeltoe.Common.HealthChecks;
+using Steeltoe.Management.Endpoint.Info;
 
 namespace PalTracker
 {
@@ -28,9 +32,9 @@ namespace PalTracker
         {
 			// Add framework services.
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-			 
-			// Add database services
-			services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
+			
+            // Add database services
+            services.AddDbContext<TimeEntryContext>(options => options.UseMySql(Configuration));
 			
 			services.AddSingleton(sp => new WelcomeMessage(
                Configuration.GetValue<string>("WELCOME_MESSAGE", "WELCOME_MESSAGE not configured.") 
@@ -47,7 +51,14 @@ namespace PalTracker
 		   // services.AddSingleton<ITimeEntryRepository, InMemoryTimeEntryRepository>();
 		   services.AddScoped<ITimeEntryRepository, MySqlTimeEntryRepository>();
 		   
-		}
+		   services.AddCloudFoundryActuators(Configuration);
+
+           // add health check
+           services.AddScoped<IHealthContributor, TimeEntryHealthContributor>();
+           services.AddSingleton<IOperationCounter<TimeEntry>, OperationCounter<TimeEntry>>();
+		   services.AddSingleton<IInfoContributor, TimeEntryInfoContributor>();
+		   
+       }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
@@ -63,6 +74,7 @@ namespace PalTracker
 
             app.UseHttpsRedirection();
             app.UseMvc();
+			app.UseCloudFoundryActuators();
         }
     }
 }
